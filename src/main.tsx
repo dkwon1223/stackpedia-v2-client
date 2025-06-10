cf7962539213a3e710792cb5f3660d575af2deb4
David Kwon <112133897+dkwon1223@users.noreply.github.com>
2025-06-09 19:12:32 -0600

    Add axios and tanstack query dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import RootLayout from './pages/RootLayout.tsx'
-import {
-  createBrowserRouter,
-  RouterProvider
-} from 'react-router';
-import Home from './pages/Home.tsx';
-import Technologies from './pages/Technologies.tsx';
-import { ThemeProvider } from './context/ThemeContext';
-import Profile from './pages/Profile.tsx';
-import Settings from './pages/Settings.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import RootLayout from "./pages/RootLayout.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./pages/Home.tsx";
+import Technologies from "./pages/Technologies.tsx";
+import { ThemeProvider } from "./context/ThemeContext";
+import Profile from "./pages/Profile.tsx";
+import Settings from "./pages/Settings.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
     children: [
-      { path: "*", Component: Home }, 
+      { path: "*", Component: Home },
       { index: true, Component: Home },
       { path: "technologies", Component: Technologies },
       { path: "profile", Component: Profile },
-      { path: "settings", Component: Settings }
-    ]
-  }
-])
+      { path: "settings", Component: Settings },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </StrictMode>
+);
